90cda56f0eeaa88a0d69c090978ad450504c5e51
Landon Reed <landontreed@gmail.com>
2020-12-16 16:27:00 -0500

    ci(travis): use chocolatey path for maven

--- windows-install-java.sh
+++ windows-install-java.sh
@@ -1,9 +1,9 @@
 export JAVA_HOME=${JAVA_HOME:-/c/jdk}
-export MAVEN_HOME=${MAVEN_HOME:-/c/maven}
+export MAVEN_HOME=${MAVEN_HOME:-/c/ProgramData/chocolatey/lib/maven}
 export PATH=${MAVEN_HOME}/bin:${JAVA_HOME}/bin:${PATH}
 echo $PATH
 # Install java and maven
-choco install jdk8 --install-directory 'c:\\jdk' -y
+choco install jdk8 -params 'installdir=c:\\jdk' -y
 java -version
-choco install maven --install-directory 'c:\\maven'
+choco install maven -y
 mvn --version

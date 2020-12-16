951db3503ae719396ddb848f051324887463f9a1
Landon Reed <landontreed@gmail.com>
2020-12-16 15:49:42 -0500

    ci(windows): fix installation of maven

--- windows-install-java.sh
+++ windows-install-java.sh
@@ -1,7 +1,6 @@
 export JAVA_HOME=${JAVA_HOME:-/c/jdk}
+export MAVEN_HOME=${MAVEN_HOME:-/c/maven}
 export PATH=${JAVA_HOME}/bin:${PATH}
 # Install java and maven
 choco install jdk8 -params 'installdir=c:\\jdk' -y
-choco install maven
-# Refresh shell (so that PATH variable changed by choco is valid)
-refreshenv
+choco install maven -params 'installdir=c:\\maven' -y

1eb6ddb2c3c4c5bac051fc9af295a29d968e7a54
Landon Reed <landontreed@gmail.com>
2020-12-16 15:29:02 -0500

    ci(windows-install-java): refresh shell for PATH variable changes

--- windows-install-java.sh
+++ windows-install-java.sh
@@ -1,4 +1,7 @@
 export JAVA_HOME=${JAVA_HOME:-/c/jdk}
 export PATH=${JAVA_HOME}/bin:${PATH}
+# Install java and maven
 choco install jdk8 -params 'installdir=c:\\jdk' -y
 choco install maven
+# Refresh shell (so that PATH variable changed by choco is valid)
+refreshenv

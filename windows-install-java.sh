ae1ac11d42139e134101a12eabe6068e0767e21f
Landon Reed <landontreed@gmail.com>
2020-12-16 16:52:43 -0500

    ci(windows): fix maven install

--- windows-install-java.sh
+++ windows-install-java.sh
@@ -1,10 +1,4 @@
 export JAVA_HOME=${JAVA_HOME:-/c/jdk}
-export MAVEN_HOME=${MAVEN_HOME:-/c/ProgramData/chocolatey/lib/maven}
-export PATH=${MAVEN_HOME}/bin:${JAVA_HOME}/bin:${PATH}
-echo $PATH
-# Install java and maven
+export PATH=${JAVA_HOME}/bin:${PATH}
+# Install java
 choco install jdk8 -params 'installdir=c:\\jdk' -y
-choco install maven -y
-# Call RefreshEnv so that environment variable updates are recognized.
-# https://travis-ci.community/t/install-location-for-iisexpress/2902/2?u=landonreed
-cmd.exe //c "RefreshEnv.cmd"

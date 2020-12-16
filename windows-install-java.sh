00130f9a5d757a578f69c0a8aa2b910277efbd9a
Landon Reed <landontreed@gmail.com>
2020-12-16 16:23:49 -0500

    ci(travis): fix install directory param

--- windows-install-java.sh
+++ windows-install-java.sh
@@ -1,9 +1,9 @@
 export JAVA_HOME=${JAVA_HOME:-/c/jdk}
 export MAVEN_HOME=${MAVEN_HOME:-/c/maven}
-export PATH=${JAVA_HOME}/bin:${JAVA_HOME}/bin:${PATH}
+export PATH=${MAVEN_HOME}/bin:${JAVA_HOME}/bin:${PATH}
 echo $PATH
 # Install java and maven
-choco install jdk8 -params 'installdir=c:\\jdk' -y
+choco install jdk8 --install-directory 'c:\\jdk' -y
 java -version
-choco install maven -params 'installdir=c:\\maven' -y
-mvn -version
+choco install maven --install-directory 'c:\\maven'
+mvn --version
